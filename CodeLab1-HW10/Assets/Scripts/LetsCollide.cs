ca1f826b7bcd1bdae58a6b56a7fceaaed6e5f92a
Amber <415411212@qq.com>
2019-05-11 22:09:05 -0400

    Finally updated my week 10 homework... Sorry for the delay...

---
+++ CodeLab1-HW10/Assets/Scripts/LetsCollide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetsCollide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start ()
+    {
+        InvokeRepeating("Spawn", 3, 5f); //Call Spawn after 3 seconds and then every half second
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+    }
+
+    void Spawn()
+    {
+        GameObject newFollower = Instantiate(Resources.Load<GameObject>("Prefabs/Follower"));
+        newFollower.transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10,10));
+    }
+}
